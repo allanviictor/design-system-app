a42323efa35985fba29ab04bbe0e76e6a06ca183
allan victor de moura <allanviictor07@gmail.com>
2026-04-05 23:09:43 -0300

    feat: add Toaster to app root for export notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import { ThemeProvider } from "@/lib/ThemeProvider"
+import { Toaster } from "@/shared/components/ui/sonner"
 import App from "./App.tsx"
 import "./index.css"
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Toaster position="bottom-right" richColors />
     </ThemeProvider>
   </StrictMode>
 )
